db9eabc71db5a3895d233a01c6e36c577093a9a2
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-07-25 15:50:07 +0200

    Add uploading images to Cloudinary

--- app/models/exam_image.rb
+++ app/models/exam_image.rb
@@ -1,16 +1,18 @@
 class ExamImage < ActiveRecord::Base
-  validates :dgt_url, :imgur_url, presence: true
+  validates :dgt_url, :storage_url, presence: true
 
   def self.process url
     image = find_by dgt_url: url
     return true if image
 
     image = new dgt_url: url
+    image.upload
     image.save
   end
 
   def upload
-    #Cloudinary::Uploader.upload dgt_full_url
+    c = Cloudinary::Uploader.upload dgt_full_url
+    self.storage_url = c['url']
   end
 
   def dgt_full_url
